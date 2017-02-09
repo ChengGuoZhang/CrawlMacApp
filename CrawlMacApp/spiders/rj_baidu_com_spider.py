f216b737a905cf0f35ce0207a47c8d03b1a10841
Eli <zcg0723@gmail.com>
2017-02-09 16:48:01 +0800

    add baidu spider and refactor the project which can support the extend by just add a new spider

--- CrawlMacApp/spiders/rj_baidu_com_spider.py
+++ CrawlMacApp/spiders/rj_baidu_com_spider.py
@@ -2,9 +2,11 @@
 import scrapy
 from scrapy_splash import SplashRequest
 
+from CrawlMacApp.items import CrawlmacappItem
+
 
 class BaiduCrawlSpider(scrapy.Spider):
-    name = "baidu_crawl"
+    name = "rj_baidu_com"
     allowed_domains = ["rj.baidu.com"]
     start_urls = ['http://rj.baidu.com/search/index/?kw=mac']
     base_url = start_urls[0]
@@ -18,10 +20,10 @@ class BaiduCrawlSpider(scrapy.Spider):
 
 
     def parse(self, response):
-        print "_____________________________________________________________________"
+        #print "_____________________________________________________________________"
         #page = response.body
         #page = page.decode('utf-8').encode('GB18030')
-        print "******************************************************************"
+        #print "******************************************************************"
         page_num = response.selector.xpath('//div[contains(@class,"page")]/span[contains(@class,"pageList")]/a[6]/text()').extract()[0]
         for cur_page in range(1,int(page_num)+1):
             compose_url = BaiduCrawlSpider.base_url+"&pageNum="+str(cur_page)
@@ -31,7 +33,20 @@ class BaiduCrawlSpider(scrapy.Spider):
     def parse_per_page(self,response):
         print "_____________________________________________________________________"
         download_urls =response.selector.xpath('//div[contains(@class,"download")]/a/@href').extract()
-        print download_urls
+        for url in download_urls:
+            item = CrawlmacappItem()
+            item['soft_id'] = "111"
+            item['soft_name'] = "222"
+            item['download_link'] = url
+            item['is_download'] = False
+            item['download_time'] = ""
+            item['soft_sha256'] = ""
+            item['is_upload_ftp'] = False
+            item['upload_time'] = ""
+            item['upload_file_name'] = ""
+            item['download_http_error'] = False
+            item['soft_desc'] = "333"
+            yield item
         print "******************************************************************"
 
 
